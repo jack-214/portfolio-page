4594d4d4b910d09d64db17973fcfcf4214446e69
Jack Ho <jack.ho12345@gmail.com>
2025-07-24 14:21:01 +0100

    Modified HomePage to include icons and modified wording

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,9 +1,9 @@
 export default function HomePage() {
   return (
     <div className="flex min-h-96 flex-col items-center justify-center">
-      <p>Hi, my name is</p>
-      <h1 className="p-10 font-poppins text-6xl font-bold">Jack Ho.</h1>
-      <h2 className="text-5xl">I build and tinker things for the web.</h2>
+      <p className="text-4xl">Hi, my name is</p>
+      <h1 className="p-7 font-poppins text-8xl font-bold">Jack Ho.</h1>
+      <h2 className="text-4xl">I build and tinker things for the web.</h2>
     </div>
   )
 }
--- src/components/ProjectGrid.tsx
+++ src/components/ProjectGrid.tsx
@@ -12,31 +12,36 @@ type ProjectGridProps = {
 
 export default function ProjectGrid({ projectList }: ProjectGridProps) {
   return (
-    <div className="flex min-h-80 items-center justify-center">
-      <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {projectList.map((project: Project, index: number) => {
-          return (
-            <li key={index}>
-              <a
-                href={project.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="block transition duration-300 hover:scale-105"
-              >
-                <div className="flex size-64 flex-col justify-center gap-6 rounded-2xl border-2 border-slate-900 bg-white p-4 text-center font-poppins text-[#0b2131]">
-                  <img
-                    className="mb-2 mt-auto h-32 w-full object-contain"
-                    src={project.image}
-                    alt={project.name}
-                  />
+    <div>
+      <div className="text-center">
+        <p className="text-2xl">Some of the stuff I&apos;ve done so far...</p>
+      </div>
+      <div className="flex min-h-80 items-center justify-center">
+        <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {projectList.map((project: Project, index: number) => {
+            return (
+              <li key={index}>
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="block transition duration-300 hover:scale-105"
+                >
+                  <div className="flex size-64 flex-col justify-center gap-6 rounded-2xl border-2 border-slate-900 bg-white p-4 text-center font-poppins text-[#0b2131]">
+                    <img
+                      className="mb-1 mt-auto h-auto w-full object-contain"
+                      src={project.image}
+                      alt={project.name}
+                    />
 
-                  <p className="mt-auto">{project.name}</p>
-                </div>
-              </a>
-            </li>
-          )
-        })}
-      </ul>
+                    <p className="mt-auto">{project.name}</p>
+                  </div>
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
