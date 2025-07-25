5789b0632c5a589171649d695fb53fd42dffab02
Jack Ho <jack.ho12345@gmail.com>
2025-07-25 15:26:38 +0100

    Updates

--- src/components/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <div className="flex min-h-96 flex-col items-center justify-center">
-      <p className="text-4xl">Hi, my name is</p>
-      <h1 className="p-7 font-poppins text-8xl font-bold">Jack Ho.</h1>
-      <h2 className="text-4xl">I build and tinker things for the web.</h2>
-    </div>
-  )
-}
