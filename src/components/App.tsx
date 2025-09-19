c919d4359376982c75160618c1af5419f9b0cc1f
Jack Ho <jack.ho12345@gmail.com>
2025-09-19 17:22:42 +0100

    Add Lumo project to portfolio

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,17 +6,53 @@ import { BackgroundBeams } from './ui/BackgroundBeams'
 
 import PlantBaseThumbnail from '../assets/plant_base_thumbnail.png'
 import NorthcodersNewsThumbnail from '../assets/northcoders_thumbnail.png'
+import LumoThumbnail from '../assets/lumo_thumbnail.png'
 
 const projectList = [
   {
     name: 'Plant Base',
     link: 'https://github.com/Emiltps/plantBase-Frontend.git',
-    image: PlantBaseThumbnail
+    image: PlantBaseThumbnail,
+    description:
+      'A plant care app to help you keep track of your plants and their needs.',
+    technologies: [
+      'React Native',
+      'TypeScript',
+      'TailwindCSS',
+      'Node.js',
+      'Express',
+      'PostgreSQL'
+    ]
   },
   {
     name: 'Northcoders News',
     link: 'https://github.com/jack-214/nc-news-frontend',
-    image: NorthcodersNewsThumbnail
+    image: NorthcodersNewsThumbnail,
+    description:
+      'A news aggregation site where users can read and post articles, comment on them, and vote them up or down.',
+    technologies: [
+      'React',
+      'TypeScript',
+      'CSS Modules',
+      'Node.js',
+      'Express',
+      'PostgreSQL'
+    ]
+  },
+  {
+    name: 'Lumo',
+    link: 'https://github.com/jack-214/ai-learning-companion',
+    image: LumoThumbnail,
+    description:
+      'An AI-powered learning companion that helps users learn new topics through interactive conversations and personalised voice assistants.',
+    technologies: [
+      'Next.js',
+      'React',
+      'TypeScript',
+      'TailwindCSS',
+      'Vapi',
+      'Supabase'
+    ]
   }
 ]
 
